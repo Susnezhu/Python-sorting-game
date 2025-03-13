8216c84f0eb9817716998bb6be8906d3905ed207
Alisa Moilanen <alisa.moilanen@gmail.com>
2025-03-13 14:44:43 +0200

    Luokka roskakorit lisätty

--- roskakorit.py
+++ roskakorit.py
@@ -1,11 +1,29 @@
 import pygame
 import random
-
+ 
 from kuvien_lataus import *
+ 
+ 
+class Roskakorit:
+    def __init__(self, kuva, arvo, x, y):
+        self.kuva = kuva
+        self.arvo = arvo
+        self.x = x
+        self.y = y
+ 
+        self.reunat = self.kuva.get_rect(topleft=(x, y))
+ 
+    def lisaa(self, naytto):
+        naytto.blit(self.kuva, (self.x,self.y))
+ 
+ 
+ 
+roskakorit = [Roskakorit(bio_roskakori, 5, 700, 260),
+              Roskakorit(kartonki_roskakori, 2, 500, 260),
+              Roskakorit(lasi_roskakori, 4, 100, 200),
+              Roskakorit(metalli_roskakori, 6, 300, 200),
+              Roskakorit(muovi_roskakori, 3, 600, 270),
+              Roskakorit(paperi_roskakori, 1, 200, 200)]
 
-kartonki_reunat = kartonki_roskakori.get_rect(topleft=(500, 260))
-bio_reunat = bio_roskakori.get_rect(topleft=(700, 260))
-lasi_reunat = lasi_roskakori.get_rect(topleft=(100, 200))
-metalli_reunat = metalli_roskakori.get_rect(topleft=(300, 200))
-muovi_reunat = muovi_roskakori.get_rect(topleft=(600, 270))
-paperi_reunat = paperi_roskakori.get_rect(topleft=(200, 200))
+
+ 
