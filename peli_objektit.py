a9839493ab970847b863417a9a2a3726a8783ac1
susnezhu <susan.klimo@gmail.com>
2025-03-13 11:41:43 +0200

    moduulit siirry lisatty ja paivita muokattu

--- peli_objektit.py
+++ peli_objektit.py
@@ -6,20 +6,24 @@ from kuvien_lataus import *
 class PeliObjektit:
     def __init__(self, kuva, arvo):
         self.x = random.randint(0, 900 - kuva.get_width())
-        self.y = random.randint(350, 600-65)
+        self.y = random.randint(400, 600 - kuva.get_height())
         
         self.kuva = kuva
         self.reunat = self.kuva.get_rect(topleft=(self.x, self.y))
         
         self.arvo = arvo
+        self.valittu = False
+        self.alkuperaiset_kordinaatit = self.x, self.y
         
-    def paivita(self, dx, dy):
-        self.reunat.x += dx
-        self.reunat.y += dy
+    def paivita(self, x, y):
+        self.reunat.topleft = (x - self.kuva.get_width()/2, y - self.kuva.get_height()/2)
 
     def lisaa(self, screen):
         screen.blit(self.kuva, self.reunat.topleft)
-    
+
+    def siirry(self, x, y):
+        if self.reunat.collidepoint(x, y):
+            self.paivita(x, y)
 
 #lista, jossa on PeliObjekti luokan oliot
 objektit = (
