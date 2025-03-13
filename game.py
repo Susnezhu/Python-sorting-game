6e347a907e1570e0bbcfc49bf6873b495bd9f229
Alisa Moilanen <alisa.moilanen@gmail.com>
2025-03-13 11:36:47 +0200

    lisatty hiiri

--- game.py
+++ game.py
@@ -12,30 +12,39 @@ naytto = pygame.display.set_mode((LEVEYS, KORKEUS))
 pygame.display.set_caption("Roskien lajittelu peli")
 
 
-
+seuraa_hiirta = False
+valittu_objekti = None
 while True:
-    for tapahtuma in pygame.event.get():
-        if tapahtuma.type == pygame.MOUSEBUTTONDOWN:
-            x, y = tapahtuma.pos
-            for obj in objektit:
-                if obj.reunat.collidepoint(x, y):
-                    print("object found") #tähän pitää lisätä hiiren seuraaminen
-                    print(obj.arvo) #kertoo mihin roskikseen se menee
-
-
-        if tapahtuma.type == pygame.QUIT:
-            exit()
-
     naytto.blit(taustakuva, (0,0))
-    for objekti in objektit:
-        objekti.lisaa(naytto)
-    
-
     naytto.blit(kartonki_roskakori, (500, 260))
     naytto.blit(bio_roskakori, (700,260))
     naytto.blit(lasi_roskakori, (100, 200))
     naytto.blit(metalli_roskakori, (300, 200))
     naytto.blit(muovi_roskakori, (600, 270))
     naytto.blit(paperi_roskakori, (200, 200))
-        
+ 
+ 
+    for tapahtuma in pygame.event.get():
+        if tapahtuma.type == pygame.MOUSEBUTTONDOWN:
+            x, y = tapahtuma.pos
+            for obj in reversed(objektit):
+                if obj.reunat.collidepoint(x, y):
+                    seuraa_hiirta = True
+                    valittu_objekti = obj
+                    break
+        if tapahtuma.type == pygame.MOUSEBUTTONUP:
+            seuraa_hiirta = False
+            valittu_objekti = None
+ 
+        if tapahtuma.type == pygame.MOUSEMOTION and seuraa_hiirta:
+            x,y = tapahtuma.pos
+            if valittu_objekti:
+                valittu_objekti.siirry(x, y)
+ 
+        if tapahtuma.type == pygame.QUIT:
+            exit()
+ 
+    for objekti in objektit:
+        objekti.lisaa(naytto)
+ 
     pygame.display.flip()
