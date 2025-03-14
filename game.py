6f5b977e2e574dd6ed42ed0bcd6ec578b83136dc
Alisa Moilanen <alisa.moilanen@gmail.com>
2025-03-14 11:43:00 +0200

    roskat tippuu roskikseen

--- game.py
+++ game.py
@@ -1,47 +1,78 @@
 #Pelin koodi kirjoitetaan tähän
 import pygame
-
+ 
 pygame.init()
-
+ 
 from kuvien_lataus import * #tiedosto, jossa on kaikki kuvat
 from peli_objektit import * #tiedosto, jossa on luokka PeliObjektit
-from roskakorit import *
-
+from roskakorit import * #tiedosto, jossa on luokka Roskakorit
+ 
 LEVEYS = 900
 KORKEUS = 600
 naytto = pygame.display.set_mode((LEVEYS, KORKEUS))
 pygame.display.set_caption("Roskien lajittelu peli")
-
-
+ 
+ 
+pisteet = 0
+ 
+peli_loppui = False
+ 
+def uusi_peli():
+    global peli_loppui, objektit, objektit_kopio, pisteet
+    peli_loppui = False
+    objektit = objektit_kopio
+    pisteet = 0
+ 
+ 
 seuraa_hiirta = False
 valittu_objekti = None
 while True:
     naytto.blit(taustakuva, (0,0))
-    
+ 
     for roskakori in roskakorit:
         roskakori.lisaa(naytto)
  
+    if objektit == []:
+        peli_loppui = True
+ 
+ 
     for tapahtuma in pygame.event.get():
-        if tapahtuma.type == pygame.MOUSEBUTTONDOWN:
-            x, y = tapahtuma.pos
-            for obj in reversed(objektit):
-                if obj.reunat.collidepoint(x, y):
-                    seuraa_hiirta = True
-                    valittu_objekti = obj
+        if tapahtuma.type == pygame.KEYDOWN:
+                if peli_loppui and tapahtuma.key == pygame.K_r:
+                    uusi_peli()
+        if not peli_loppui:
+            if tapahtuma.type == pygame.MOUSEBUTTONDOWN:
+                x, y = tapahtuma.pos
+                for obj in reversed(objektit):
+                    if obj.reunat.collidepoint(x, y):
+                        seuraa_hiirta = True
+                        valittu_objekti = obj
+                        break
+            if tapahtuma.type == pygame.MOUSEBUTTONUP:
+                seuraa_hiirta = False
+                if valittu_objekti == None:
                     break
-        if tapahtuma.type == pygame.MOUSEBUTTONUP:
-            seuraa_hiirta = False
-            valittu_objekti = None
+                for roska in roskakorit:
+                    if roska.reunat.collidepoint(valittu_objekti.reunat.x, valittu_objekti.reunat.y):
+                            valittu_objekti.poista(objektit)
+                            if valittu_objekti.arvo == roska.arvo:
+                                pisteet += 1
  
-        if tapahtuma.type == pygame.MOUSEMOTION and seuraa_hiirta:
-            x,y = tapahtuma.pos
-            if valittu_objekti:
-                valittu_objekti.siirry(x, y)
  
+                valittu_objekti = None
+    
+            if tapahtuma.type == pygame.MOUSEMOTION and seuraa_hiirta:
+                x,y = tapahtuma.pos
+                if valittu_objekti:
+                    valittu_objekti.siirry(x, y)
+    
         if tapahtuma.type == pygame.QUIT:
             exit()
  
-    for objekti in objektit:
-        objekti.lisaa(naytto)
  
-    pygame.display.flip()
+ 
+    if not peli_loppui:
+        for objekti in objektit:
+            objekti.lisaa(naytto)
+    
+        pygame.display.flip()
--- peli_objektit.py
+++ peli_objektit.py
@@ -1,8 +1,9 @@
+
 import pygame
 import random
-
+ 
 from kuvien_lataus import *
-
+ 
 class PeliObjektit:
     def __init__(self, kuva, arvo):
         self.x = random.randint(0, 900 - kuva.get_width())
@@ -23,9 +24,21 @@ class PeliObjektit:
     def siirry(self, x, y):
         if self.reunat.collidepoint(x, y):
             self.paivita(x, y)
+ 
+    def poista(self, lista):
+        if self in lista:
+            lista.remove(self)
             
 #lista, jossa on PeliObjekti luokan oliot
 objektit = (
+    [PeliObjektit(roska, 1) for roska in paperi] +
+    [PeliObjektit(roska, 2) for roska in kartonki] +
+    [PeliObjektit(roska, 3) for roska in muovi] +
+    [PeliObjektit(roska, 4) for roska in lasi] +
+    [PeliObjektit(roska, 5) for roska in bio] +
+    [PeliObjektit(roska, 6) for roska in metalli]
+)
+objektit_kopio = (
     [PeliObjektit(roska, 1) for roska in paperi] +
     [PeliObjektit(roska, 2) for roska in kartonki] +
     [PeliObjektit(roska, 3) for roska in muovi] +
